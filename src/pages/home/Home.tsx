5a33479a3aebf58a21f3fd1c7ddad862c7d36a8f
mateusriff <mateusnriff@gmail.com>
2025-07-13 22:11:58 -0300

    chore: restructure repo

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+
+import Banner from "../../components/banner/Banner"
+
+
+export default function() {
+    
+    return(
+        <>
+            Olá, mundo
+
+            <Banner />
+        </>
+    )
+}
