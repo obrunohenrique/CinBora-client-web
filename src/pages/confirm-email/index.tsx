bf9bae650520a6e7e5364075e8f578e6ed88fb12
Bruno Henrique <brunohenriquef.pinto@gmail.com>
2025-07-27 01:17:09 -0300

    estilizei a tela de login, cadastro e recuperação de senha, adicionanco favicon no index.html, mudei o nome da guia da página

--- src/pages/confirm-email/index.tsx
+++ src/pages/confirm-email/index.tsx
@@ -1,59 +1,75 @@
 import type { FormProps } from 'antd';
 import { Button, Flex, Form, Input } from 'antd';
+import './confirm-email-page.css';
+import logomax from '../../../public/cinbora-logo-max.png'
+
 
 type FieldType = {
-  email?: string;
-  password?: string;
-  remember?: string;
+	email?: string;
+	password?: string;
+	remember?: string;
 };
 
 const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  console.log('Success:', values);
+	console.log('Success:', values);
 };
 
 const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
+	console.log('Failed:', errorInfo);
 };
 
 const ConfirmEmailPage = () => {
-  return (
-		<Form
-			name="basic"
-			labelCol={{ span: 8 }}
-			wrapperCol={{ span: 16 }}
-			style={{ maxWidth: 600 }}
-			initialValues={{ remember: true }}
-			onFinish={onFinish}
-			onFinishFailed={onFinishFailed}
-			autoComplete="off"
-		>
-			<Form.Item
-				label="Código de verificação"
-				name="verificationCode"
-				rules={[
-					{ required: true, message: 'Insira o código de verificação!' },
-					{ min: 6, message: 'O código deve ter pelo menos 6 caracteres!' }
-				]}
-				labelCol={{ span: 24 }}
-				wrapperCol={{ span: 24 }}
-			>
-				<Input.OTP />
-			</Form.Item>
+	return (
+		<div className='main-recuperar-senha'>
+			<div className='main-recuperar-senha-sobreposicao'>
 
-			<Flex vertical={false} justify='space-between'>
-				<Form.Item label={null} wrapperCol={{ span: 24 }}>
-					<Button type="primary" htmlType="submit">
-						Verificar
-					</Button>
-				</Form.Item>
-				<Form.Item label={null} wrapperCol={{ span: 24 }}>
-					<Button type="text" htmlType="submit">
-						Reenviar
-					</Button>
-				</Form.Item>
-			</Flex>
-		</Form>
-  )
+
+				<Form
+					name="basic"
+					className="estrutura-formulario-verificacao"
+					labelCol={{ span: 8 }}
+					wrapperCol={{ span: 16 }}
+					style={{ maxWidth: 600 }}
+					initialValues={{ remember: true }}
+					onFinish={onFinish}
+					onFinishFailed={onFinishFailed}
+					autoComplete="off"
+				>
+
+					<div className='logo-login-container'>
+						<img src={logomax} alt="logo cinbora" className='logo-cinbora-login' />
+					</div>
+
+					<Form.Item
+						className="campo-codigo-verificacao"
+						label="Código de verificação"
+						name="verificationCode"
+						rules={[
+							{ required: true, message: 'Insira o código de verificação!' },
+							{ min: 6, message: 'O código deve ter pelo menos 6 caracteres!' }
+						]}
+						labelCol={{ span: 24 }}
+						wrapperCol={{ span: 24 }}
+					>
+						<Input.OTP className="input-codigo-estilizado" />
+					</Form.Item>
+
+					<Flex vertical={false} justify='space-between' className="area-acoes-verificacao">
+						<Form.Item label={null} wrapperCol={{ span: 24 }} className="botao-verificar-container">
+							<Button type="primary" htmlType="submit" className="botao-verificar">
+								Verificar
+							</Button>
+						</Form.Item>
+						<Form.Item label={null} wrapperCol={{ span: 24 }} className="botao-reenviar-container">
+							<Button type="text" htmlType="submit" className="botao-reenviar">
+								Reenviar
+							</Button>
+						</Form.Item>
+					</Flex>
+				</Form>
+			</div>
+		</div>
+	)
 }
 
-export default ConfirmEmailPage
+export default ConfirmEmailPage;
